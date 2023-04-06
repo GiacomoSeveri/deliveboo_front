88f072b5b79223db4c463967ea9fc8be4974552c
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-04-06 13:12:38 +0200

    TO DO filter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script>
 import axios from 'axios';
-const apiUrlRestaurants = 'http://127.0.0.1:8000/api/restaurants'
-const apiUrlTypes = 'http://127.0.0.1:8000/api/types'
+const apiUrlRestaurants = 'http://127.0.0.1:8000/api/restaurants';
+const apiUrlTypes = 'http://127.0.0.1:8000/api/types';
 
-import AppHeader from './components/AppHeader.vue'
-import TitleHeader from './components/TitleHeader.vue'
-import SerchBar from './components/SerchBar.vue'
-import CheckBoxCard from './components/CheckBoxCard.vue'
-import AppRestaurant from './components/AppRestaurant.vue'
+import AppHeader from './components/AppHeader.vue';
+import TitleHeader from './components/TitleHeader.vue';
+import SerchBar from './components/SerchBar.vue';
+import CheckBoxCard from './components/CheckBoxCard.vue';
+import AppRestaurant from './components/AppRestaurant.vue';
+
+import { store } from './data/store';
 export default {
     name: 'App',
     components: { AppHeader, TitleHeader, SerchBar, CheckBoxCard, AppRestaurant },
@@ -15,7 +17,8 @@ export default {
         return {
             res_types: [],
             restaurants: [],
-            checked: []
+            checked: [],
+            store
         }
     },
     methods: {
@@ -26,17 +29,29 @@ export default {
             axios.get(apiUrlRestaurants).then(res => { this.restaurants = res.data })
         },
         checkedValue(value) {
-            if (value != this.restaurants.types) {
-                this.restaurants
+            if (store.selected_types.includes(value)) {
+
+                let contain = false
+                store.selected_types.forEach((single_type, i) => {
+                    //se il value è contenuto nello store selected_type lo tolgo
+                    if (single_type == value) {
+                        store.selected_types.splice(i, 1);
+                        console.log(store.selected_types);
+                        contain = true
+
+
+                    }
+                });
+                if (contain) return
+                store.selected_types.push(value);
+                console.log(store.selected_types)
             }
+            store.selected_types.push(value);
+            console.log('quetso =>', store.selected_types[store.selected_types.length - 1])
         }
 
     },
-    computed: {
-        // checkedTypes(){
-        //     return
-        // }
-    },
+
     created() {
         this.fetchTypes();
         this.fetchRestaurants();
--- src/components/AppRestaurant.vue
+++ src/components/AppRestaurant.vue
@@ -1,12 +1,53 @@
 <script>
+import { store } from '../data/store'
 export default {
     name: 'AppRestaurant',
-    props: { restaurant: Object }
+    props: { restaurant: Object },
+    data() {
+        return {
+            store
+        }
+    },
+
+
+    methods: {
+
+        has_type(lol) {
+
+            //funzione che viene eseguita quandon nello store viene cambiato l'array
+            //type_selected con nuovi campi
+
+
+            //controllo se non sono selezionati  types
+
+            if (store.selected_types[0] == undefined) {
+                return true
+            }
+            let variabile = false;
+            let variabile_fine_ciclo = false;
+            //store.selected_types.forEach((element, index) => {
+            //    console.log('selected element =>', element);
+            //    lol.forEach((lol_element, lol_index) => {
+            //        console.log('selected elemente =>', element);
+            //
+            //   })
+            //})
+            if (store.selected_types[0] === lol[0].name) {
+                console.log('passato')
+                return true
+            }
+
+            // esegue la funzione search_type(con parametro il ristorante)
+
+            return false
+        }
+    },
+
 }
 </script>
 
 <template>
-    <div class="col-4 my-3">
+    <div class="col-4 my-3 questo" v-if="has_type(restaurant.types)">
         <div class="card">
             <img :src="restaurant.image" class="img-fluid img-res-custom" :alt="restaurant.name">
             <div class="card-body">
